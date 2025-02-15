287a5a3f4b66ffbfe8e585f9883ba585346ce4cd
Shigma <shigma10826@gmail.com>
2025-02-15 22:05:52 +0800

    feat: setup macro

---
+++ umili/src/lib.rs
@@ -0,0 +1,135 @@
+use std::ops::{Deref, DerefMut};
+use std::rc::Rc;
+use std::cell::RefCell;
+
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[cfg(feature = "derive")]
+extern crate umili_derive;
+
+pub struct Observer<'i, T: Clone + Serialize + PartialEq> {
+    pub value: &'i mut T,
+    pub path: String,
+    pub diff: Rc<RefCell<Vec<Delta>>>,
+}
+
+impl<'i, T: Clone + Serialize + PartialEq> Deref for Observer<'i, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.value
+    }
+}
+
+impl<'i, T: Clone + Serialize + PartialEq> Observer<'i, T> {
+    pub fn borrow_mut(&mut self) -> Ref<T> {
+        Ref {
+            old_value: None,
+            value: self.value,
+            path: &self.path,
+            diff: self.diff.clone(),
+        }
+    }
+}
+
+pub struct Ref<'i, T: Clone + Serialize + PartialEq> {
+    pub old_value: Option<T>,
+    pub value: &'i mut T,
+    pub path: &'i str,
+    pub diff: Rc<RefCell<Vec<Delta>>>,
+}
+
+impl<'i> Ref<'i, String> {
+    pub fn add_assign(&mut self, s: &str) {
+        self.push_str(s);
+    }
+
+
+    pub fn push(&mut self, c: char) {
+        self.diff.borrow_mut().push(Delta::APPEND {
+            p: self.path.to_string(),
+            v: serde_json::to_value(c).unwrap(),
+        });
+        self.value.push(c);
+    }
+
+    pub fn push_str(&mut self, s: &str) {
+        if s.is_empty() {
+            return;
+        }
+        self.diff.borrow_mut().push(Delta::APPEND {
+            p: self.path.to_string(),
+            v: serde_json::to_value(s).unwrap(),
+        });
+        self.value.push_str(s);
+    }
+}
+
+impl<'i, T: Clone + Serialize + PartialEq> Ref<'i, Vec<T>> {
+    pub fn push(&mut self, value: T) {
+        self.diff.borrow_mut().push(Delta::APPEND {
+            p: self.path.to_string(),
+            v: serde_json::to_value(vec![&value]).unwrap(),
+        });
+        self.value.push(value);
+    }
+
+    pub fn extend(&mut self, other: Vec<T>) { // FIXME iter
+        if other.is_empty() {
+            return;
+        }
+        self.diff.borrow_mut().push(Delta::APPEND {
+            p: self.path.to_string(),
+            v: serde_json::to_value(&other).unwrap(),
+        });
+        self.value.extend(other);
+    }
+}
+
+impl<'i, T: Clone + Serialize + PartialEq> Deref for Ref<'i, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.value
+    }
+}
+
+impl<'i, T: Clone + Serialize + PartialEq> DerefMut for Ref<'i, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        if self.old_value.is_none() {
+            self.old_value.replace(self.value.clone());
+        }
+        self.value
+    }
+}
+
+impl<'i, T: Clone + Serialize + PartialEq> Drop for Ref<'i, T> {
+    fn drop(&mut self) {
+        if let Some(old_value) = self.old_value.take() {
+            if old_value != *self.value {
+                self.diff.borrow_mut().push(Delta::SET {
+                    p: self.path.to_string(),
+                    v: serde_json::to_value(&self.value).unwrap(),
+                });
+            }
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "o")]
+pub enum Delta {
+    SET { p: String, v: Value },
+    APPEND { p: String, v: Value },
+    BATCH { p: String, v: Vec<Delta> },
+    HISTORY { p: String, v: DeltaTag },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum DeltaTag {
+    SET,
+    APPEND,
+    BATCH,
+    HISTORY,
+}
--- umili/src/main.rs
+++ umili/src/main.rs
@@ -1,7 +1,7 @@
-use std::{cell::RefCell, ops::{AddAssign, Deref, DerefMut}, rc::Rc};
+use std::{cell::RefCell, ops::AddAssign, rc::Rc};
 
 use serde::{Serialize, Deserialize};
-use serde_json::Value;
+use umili::{Delta, Observer};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct Foo {
@@ -13,12 +13,12 @@ pub struct Foo {
 impl Foo {
     pub fn as_mut(&mut self, prefix: &str, diff: &Rc<RefCell<Vec<Delta>>>) -> FooMut {
         FooMut {
-            bar: Observable {
+            bar: Observer {
                 value: &mut self.bar,
                 path: prefix.to_string() + "bar",
                 diff: diff.clone(),
             },
-            qux: Observable {
+            qux: Observer {
                 value: &mut self.qux,
                 path: prefix.to_string() + "qux",
                 diff: diff.clone(),
@@ -42,7 +42,7 @@ pub struct Bar {
 impl Bar {
     pub fn as_mut(&mut self, prefix: &str, diff: &Rc<RefCell<Vec<Delta>>>) -> BarMut {
         BarMut {
-            baz: Observable {
+            baz: Observer {
                 value: &mut self.baz,
                 path: prefix.to_string() + "baz",
                 diff: diff.clone(),
@@ -59,148 +59,22 @@ impl Bar {
 
 // 由 derive macro 生成
 pub struct FooMut<'i> {
-    pub bar: Observable<'i, Bar>,
-    pub qux: Observable<'i, String>,
+    pub bar: Observer<'i, Bar>,
+    pub qux: Observer<'i, String>,
 }
 
 // 由 derive macro 生成
 pub struct BarMut<'i> {
-    pub baz: Observable<'i, i32>,
-}
-
-pub struct Observable<'i, T: Clone + Serialize + PartialEq> {
-    pub value: &'i mut T,
-    pub path: String,
-    pub diff: Rc<RefCell<Vec<Delta>>>,
-}
-
-impl<'i, T: Clone + Serialize + PartialEq> Deref for Observable<'i, T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        self.value
-    }
-}
-
-impl<'i, T: Clone + Serialize + PartialEq> Observable<'i, T> {
-    pub fn borrow_mut(&mut self) -> Ref<T> {
-        Ref {
-            old_value: None,
-            value: self.value,
-            path: &self.path,
-            diff: self.diff.clone(),
-        }
-    }
+    pub baz: Observer<'i, i32>,
 }
 
 // 由 derive macro 生成
-impl<'i> Observable<'i, Bar> {
+impl<'i> Observer<'i, Bar> {
     #[inline]
     pub fn borrow(&mut self) -> BarMut {
         let prefix = self.path.to_string() + "/";
         self.value.as_mut(&prefix, &self.diff)
     }
-}
-
-pub struct Ref<'i, T: Clone + Serialize + PartialEq> {
-    pub old_value: Option<T>,
-    pub value: &'i mut T,
-    pub path: &'i str,
-    pub diff: Rc<RefCell<Vec<Delta>>>,
-}
-
-impl<'i> Ref<'i, String> {
-    pub fn add_assign(&mut self, s: &str) {
-        self.push_str(s);
-    }
-
-
-    pub fn push(&mut self, c: char) {
-        self.diff.borrow_mut().push(Delta::APPEND {
-            p: self.path.to_string(),
-            v: serde_json::to_value(c).unwrap(),
-        });
-        self.value.push(c);
-    }
-
-    pub fn push_str(&mut self, s: &str) {
-        if s.is_empty() {
-            return;
-        }
-        self.diff.borrow_mut().push(Delta::APPEND {
-            p: self.path.to_string(),
-            v: serde_json::to_value(s).unwrap(),
-        });
-        self.value.push_str(s);
-    }
-}
-
-impl<'i, T: Clone + Serialize + PartialEq> Ref<'i, Vec<T>> {
-    pub fn push(&mut self, value: T) {
-        self.diff.borrow_mut().push(Delta::APPEND {
-            p: self.path.to_string(),
-            v: serde_json::to_value(vec![&value]).unwrap(),
-        });
-        self.value.push(value);
-    }
-
-    pub fn extend(&mut self, other: Vec<T>) { // FIXME iter
-        if other.is_empty() {
-            return;
-        }
-        self.diff.borrow_mut().push(Delta::APPEND {
-            p: self.path.to_string(),
-            v: serde_json::to_value(&other).unwrap(),
-        });
-        self.value.extend(other);
-    }
-}
-
-impl<'i, T: Clone + Serialize + PartialEq> Deref for Ref<'i, T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        self.value
-    }
-}
-
-impl<'i, T: Clone + Serialize + PartialEq> DerefMut for Ref<'i, T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        if self.old_value.is_none() {
-            self.old_value.replace(self.value.clone());
-        }
-        self.value
-    }
-}
-
-impl<'i, T: Clone + Serialize + PartialEq> Drop for Ref<'i, T> {
-    fn drop(&mut self) {
-        if let Some(old_value) = self.old_value.take() {
-            if old_value != *self.value {
-                self.diff.borrow_mut().push(Delta::SET {
-                    p: self.path.to_string(),
-                    v: serde_json::to_value(&self.value).unwrap(),
-                });
-            }
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(tag = "o")]
-pub enum Delta {
-    SET { p: String, v: Value },
-    APPEND { p: String, v: Value },
-    BATCH { p: String, v: Vec<Delta> },
-    HISTORY { p: String, v: DeltaTag },
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub enum DeltaTag {
-    SET,
-    APPEND,
-    BATCH,
-    HISTORY,
 }
 
 fn main() {
